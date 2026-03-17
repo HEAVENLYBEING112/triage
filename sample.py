62b7bebf8ee5f71802861530e0bee548970c1a4d
SURYA112 <tamilsurya783@gmail.com>
2026-03-17 22:13:02 +0530

    fix: auto-patch sample.py by The Reproducer
    The code was manually adding numbers in a loop instead of using the built-in Python sum() function.

--- sample.py
+++ sample.py
@@ -1,9 +1,7 @@
+# --- sample.py ---
 numbers = [10, 20, 30, 40, 50]
 
-total = 0
-
-for i in range(len(numbers)):
-    total = total + numbers[i]
+total = sum(numbers)
 
 average = total / len(numbers)
 
